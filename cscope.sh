43cdb125218223f9deecd6563228ad551b3b87a5
Hansome wang <860662314@qq.com>
2018-07-11 10:17:45 +0800

    update autocomplpop

--- cscope.sh
+++ cscope.sh
@@ -2,3 +2,4 @@
 
 find \( -path './out' -o -path './.repo' -o -path './tools' \) -prune -o -type f -a \( -name "*.h" -o -name "*.c" -o -name "*.java" -o -name "*.mk" -o -name "*.xml" -o -name "*.cpp" -o -name "*.s" \) -print > cscope.files
 cscope -bq
+ctags -R --c-kinds=+px --fields=+iaS --extra=+q
